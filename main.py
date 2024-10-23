f1a0f02543b315385bc157795e275d051b9e4fa2
Patrick Hammer <patham9@gmail.com>
2024-10-23 15:35:29 +0200

    Update: condition check was missing

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ def Step(inject_key=""):
             BRIDGE_Input(METTA, observed_world, NACEToNARS = False)
             if not METTA.endswith("?") and not METTA.endswith("? :|:") and not METTA.startswith("!(EternalQuestion ") and not METTA.startswith("!(EventQuestion "):
                 asked = False
-    if "spaces" in sys.argv:
+    if interactiveWorld:
         BRIDGE_Tick(observed_world)
     start_time = time.time()
     usedRules, FocusSet, RuleEvidence, loc, observed_world, rules, negrules, world, debuginput, values, lastplanworld, planworld, behavior, plan = NACE_Cycle(Time, FocusSet, RuleEvidence, loc, observed_world, rules, negrules, deepcopy(world), inject_key)
